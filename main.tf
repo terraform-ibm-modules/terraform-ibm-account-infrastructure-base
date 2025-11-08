1e0af6435000b3839b94217839c7568ef051ae14
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-11-08 04:42:44 +0000

    fix(deps): update dependencies

--- main.tf
+++ main.tf
@@ -124,7 +124,7 @@ locals {
 module "resource_group" {
   for_each            = local.rg_map
   source              = "terraform-ibm-modules/resource-group/ibm"
-  version             = "1.3.0"
+  version             = "1.4.0"
   resource_group_name = each.key
 }
 
@@ -136,7 +136,7 @@ moved {
 module "existing_resource_group" {
   for_each                     = local.existing_rg_map
   source                       = "terraform-ibm-modules/resource-group/ibm"
-  version                      = "1.3.0"
+  version                      = "1.4.0"
   existing_resource_group_name = each.key
 }
 
@@ -148,7 +148,7 @@ moved {
 module "account_settings" {
   count                        = !var.skip_iam_account_settings ? 1 : 0
   source                       = "terraform-ibm-modules/iam-account-settings/ibm"
-  version                      = "2.12.0"
+  version                      = "2.12.8"
   access_token_expiration      = var.access_token_expiration
   active_session_timeout       = var.active_session_timeout
   allowed_ip_addresses         = var.allowed_ip_addresses
@@ -189,7 +189,7 @@ module "trusted_profile_projects" {
 module "cbr_fscloud" {
   count                                  = var.provision_cbr ? 1 : 0
   source                                 = "terraform-ibm-modules/cbr/ibm//modules/fscloud"
-  version                                = "1.33.5"
+  version                                = "1.33.8"
   prefix                                 = var.cbr_prefix
   zone_vpc_crn_list                      = []
   allow_cos_to_kms                       = var.cbr_allow_cos_to_kms
